70223d6e61294c71a84a0953f200b4f4c2ccadb4
Brahvim <69293652+Brahvim@users.noreply.github.com>
2025-03-13 20:15:26 +0530

    "Initial commit"? This is a non-GitHub template!

---
+++ include/library/library.h
@@ -0,0 +1,20 @@
+#pragma once
+
+struct SmlVec2 {
+
+	float x, y;
+
+};
+
+struct SmlVec3 {
+
+	float x, y, z;
+
+};
+
+struct SmlQuat {
+
+	float x, y, z, w;
+
+};
+
---
+++ test/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "library/library.h"
+
+int main(int const p_count, char *p_values[]) {
+	a();
+	return EXIT_SUCCESS;
+}
